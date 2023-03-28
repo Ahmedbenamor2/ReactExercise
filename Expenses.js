692b9e9159086362da1faf1df12d5c3f7a1ff0d3
Ahmedbenamor2 <91230308+Ahmedbenamor2@users.noreply.github.com>
2023-03-28 18:03:36 +0100

    Create Expenses.js

---
+++ Expenses.js
@@ -0,0 +1,30 @@
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses(props) {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={props.array[0].title}
+        amount={"$" + props.array[0].amount}
+        date={props.array[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.array[1].title}
+        amount={"$" + props.array[1].amount}
+        date={props.array[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.array[2].title}
+        amount={"$" + props.array[2].amount}
+        date={props.array[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.array[3].title}
+        amount={"$" + props.array[3].amount}
+        date={props.array[3].date}
+      ></ExpenseItem>
+    </div>
+  );
+}
+
+export default Expenses;
